cb85eae7072b66a26d2c234e3abc7696be9da92e
Praveen Sathelli <praveensathelli11@gmail.com>
2026-08-23 20:56:31 +0530

    input and required changes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,12 +12,13 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // selectedUser = signal(DUMMY_USERS[randomIndex]);
-  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
-  // }
+  @Input({required:true}) avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectuser() {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
