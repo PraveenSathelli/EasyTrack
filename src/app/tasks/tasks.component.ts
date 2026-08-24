1cbb7cf722dc389235390295ecde9e9cd6e2126d
Praveen Sathelli <praveensathelli11@gmail.com>
2026-08-24 09:52:11 +0530

    model interaces, task component introduced and filter

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,60 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) name!: string;
 
-  @Input({required:true}) name!:string ;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Lear all the basic and advanced features',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master 2 Angular',
+      summary: 'Lear features',
+      dueDate: '2024-05-31'
+    }
+    ,
+    {
+      id: 't3',
+      userId: 'u1',
+      title: 'Master 3 Angular',
+      summary: 'Lear all advanced features',
+      dueDate: '2024-03-31'
+    }
+    ,
+    {
+      id: 't4',
+      userId: 'u3',
+      title: 'Master 4 Angular',
+      summary: 'Lear 4 the basic and advanced features',
+      dueDate: '2025-04-20'
+    }
+    ,
+    {
+      id: 't5',
+      userId: 'u3',
+      title: 'Master 5 Angular',
+      summary: 'Lear 5 all basic and advanced features',
+      dueDate: '2024-02-11'
+    }
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter(x => x.userId === this.id);
+  }
 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,6 @@
 import { Component, computed, EventEmitter, Input, Output, signal } from '@angular/core';
+import { User } from './user.model';
 
-import { DUMMY_USERS } from '../dummy-users';
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-};
 
 @Component({
   selector: 'app-user',
