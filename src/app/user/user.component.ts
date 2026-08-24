6bb51d7ac180a784a3d34bbff9be189ce640876e
Praveen Sathelli <praveensathelli11@gmail.com>
2026-08-24 08:34:39 +0530

    types type, interface and alias

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,18 @@ import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
+// type User ={
+//     id:string;
+//     name:string;
+//     avatar:string;
+//   };
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -13,17 +25,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  @Input({required:true}) id?:string;
-  @Input({required:true}) avatar: string|undefined;
-  @Input() name!: string;
+  // @Input({required:true}) id!:string;
+  // @Input({required:true}) avatar!: string;
+  // @Input() name!: string;
+
+  //alias type
+  // @Input({required:true}) user! : {
+  //   id:string;
+  //   name:string;
+  //   avatar:string;
+  // };
+
+  @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectuser() {
-   this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
